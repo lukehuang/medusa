cbafe8ac77ea31e58705cd8b075b841cf8926b8d
gaohaoning <gaohaoning@breadtrip.com>
2016-04-29 17:07:57 +0800

    add concurrent code

---
+++ medusacode/python_demo/concurrent_programming_test/3_eventlet.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""
+蒙特卡洛算法计算圆周率
+"""
+
+from math import hypot
+from random import random
+import time
+
+def timeit(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        ret = func(*args, **kwargs)
+        stop = time.time()
+        delta = stop - start
+        print 'delta = %s' % delta
+        return ret
+    return wrapper
+
+def test(tries):
+    return sum(hypot(random(), random()) < 1 for _ in range(tries))
+
+
+@timeit
+def calc_pi(tries, n):
+    # ------------------------------------------------------------------------------------------
+    from eventlet import GreenPool
+    pool = GreenPool()
+    result = pool.imap(test, [tries]*n)  # eventlet
+    # ------------------------------------------------------------------------------------------
+    pi = 4.0 * sum(result)/(tries * n)
+    return pi
+
+pi = calc_pi(1000000, 10)
+print 'pi = %s' % pi
+# delta = 3.96094918251
+# pi = 3.1408468
---
+++ medusacode/python_demo/concurrent_programming_test/4_gevent.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""
+蒙特卡洛算法计算圆周率
+"""
+
+from math import hypot
+from random import random
+import time
+
+def timeit(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        ret = func(*args, **kwargs)
+        stop = time.time()
+        delta = stop - start
+        print 'delta = %s' % delta
+        return ret
+    return wrapper
+
+def test(tries):
+    return sum(hypot(random(), random()) < 1 for _ in range(tries))
+
+
+@timeit
+def calc_pi(tries, n):
+    # ------------------------------------------------------------------------------------------
+    import gevent
+    jobs = [gevent.spawn(test, t) for t in [tries] * n]
+    gevent.joinall(jobs)
+    result = [job.value for job in jobs]  # gevent
+    # ------------------------------------------------------------------------------------------
+    pi = 4.0 * sum(result)/(tries * n)
+    return pi
+
+pi = calc_pi(1000000, 10)
+print 'pi = %s' % pi
+# delta = 3.71042990685
+# pi = 3.1420892
