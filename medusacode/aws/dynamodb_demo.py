19c6e97755bc409e07ec3838547cd79d2da72a0a
gaohaoning <gaohaoning@unipus.cn>
2017-02-09 17:29:03 +0800

    aws dynamodb : table create&delete ok.

--- medusacode/aws/dynamodb_demo.py
+++ medusacode/aws/dynamodb_demo.py
@@ -22,6 +22,7 @@ aws_secret_access_key = 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
 
 from boto.dynamodb2.layer1 import DynamoDBConnection
 
+print '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
 # Connect to DynamoDB Local
 conn = DynamoDBConnection(
     host='192.168.33.10',
@@ -36,3 +37,161 @@ print conn
 tables = conn.list_tables()
 print tables
 # {u'TableNames': []}
+
+print '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+# Create table
+table = conn.create_table(
+    table_name='Music',
+    key_schema=[
+        {
+            'AttributeName': "Artist",
+            'KeyType': "HASH"
+        },
+        {
+            'AttributeName': "SongTitle",
+            'KeyType': "RANGE"
+        }
+    ],
+    attribute_definitions=[
+        {
+            'AttributeName': "Artist",
+            'AttributeType': "S"
+        },
+        {
+            'AttributeName': "SongTitle",
+            'AttributeType': "S"
+        }
+    ],
+    provisioned_throughput={
+        'ReadCapacityUnits': 1,
+        'WriteCapacityUnits': 1
+    }
+)
+print table
+# {
+#   u'TableDescription': {
+#     u'TableArn': u'arn:aws:dynamodb:ddblocal:000000000000:table/Music',
+#     u'AttributeDefinitions': [
+#       {
+#         u'AttributeName': u'Artist',
+#         u'AttributeType': u'S'
+#       },
+#       {
+#         u'AttributeName': u'SongTitle',
+#         u'AttributeType': u'S'
+#       }
+#     ],
+#     u'ProvisionedThroughput': {
+#       u'NumberOfDecreasesToday': 0,
+#       u'WriteCapacityUnits': 1,
+#       u'LastIncreaseDateTime': 0.0,
+#       u'ReadCapacityUnits': 1,
+#       u'LastDecreaseDateTime': 0.0
+#     },
+#     u'TableSizeBytes': 0,
+#     u'TableName': u'Music',
+#     u'TableStatus': u'ACTIVE',
+#     u'KeySchema': [
+#       {
+#         u'KeyType': u'HASH',
+#         u'AttributeName': u'Artist'
+#       },
+#       {
+#         u'KeyType': u'RANGE',
+#         u'AttributeName': u'SongTitle'
+#       }
+#     ],
+#     u'ItemCount': 0,
+#     u'CreationDateTime': 1486632161.815
+#   }
+# }
+tt = conn.list_tables()
+print tt
+# {u'TableNames': [u'Music']}
+print '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+# Describe table
+tt = conn.describe_table('Music')
+print tt
+# {
+#   u'Table': {
+#     u'TableArn': u'arn:aws:dynamodb:ddblocal:000000000000:table/Music',
+#     u'AttributeDefinitions': [
+#       {
+#         u'AttributeName': u'Artist',
+#         u'AttributeType': u'S'
+#       },
+#       {
+#         u'AttributeName': u'SongTitle',
+#         u'AttributeType': u'S'
+#       }
+#     ],
+#     u'ProvisionedThroughput': {
+#       u'NumberOfDecreasesToday': 0,
+#       u'WriteCapacityUnits': 1,
+#       u'LastIncreaseDateTime': 0.0,
+#       u'ReadCapacityUnits': 1,
+#       u'LastDecreaseDateTime': 0.0
+#     },
+#     u'TableSizeBytes': 0,
+#     u'TableName': u'Music',
+#     u'TableStatus': u'ACTIVE',
+#     u'KeySchema': [
+#       {
+#         u'KeyType': u'HASH',
+#         u'AttributeName': u'Artist'
+#       },
+#       {
+#         u'KeyType': u'RANGE',
+#         u'AttributeName': u'SongTitle'
+#       }
+#     ],
+#     u'ItemCount': 0,
+#     u'CreationDateTime': 1486632161.815
+#   }
+# }
+print '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+# Delete table
+description = conn.delete_table('Music')
+print description
+# {
+#   u'TableDescription': {
+#     u'TableArn': u'arn:aws:dynamodb:ddblocal:000000000000:table/Music',
+#     u'AttributeDefinitions': [
+#       {
+#         u'AttributeName': u'Artist',
+#         u'AttributeType': u'S'
+#       },
+#       {
+#         u'AttributeName': u'SongTitle',
+#         u'AttributeType': u'S'
+#       }
+#     ],
+#     u'ProvisionedThroughput': {
+#       u'NumberOfDecreasesToday': 0,
+#       u'WriteCapacityUnits': 1,
+#       u'LastIncreaseDateTime': 0.0,
+#       u'ReadCapacityUnits': 1,
+#       u'LastDecreaseDateTime': 0.0
+#     },
+#     u'TableSizeBytes': 0,
+#     u'TableName': u'Music',
+#     u'TableStatus': u'ACTIVE',
+#     u'KeySchema': [
+#       {
+#         u'KeyType': u'HASH',
+#         u'AttributeName': u'Artist'
+#       },
+#       {
+#         u'KeyType': u'RANGE',
+#         u'AttributeName': u'SongTitle'
+#       }
+#     ],
+#     u'ItemCount': 0,
+#     u'CreationDateTime': 1486632404.093
+#   }
+# }
+print '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+print '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+print '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+print '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
+
