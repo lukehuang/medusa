93052447c2b59c738104362ab94c4d918b165816
gaohaoning <gaohaoning@unipus.cn>
2018-04-09 18:09:42 +0800

    add code for data_science_from_scratch.

---
+++ data_science/data_science_from_scratch/03_visualizing_data.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+import numpy as np
+from matplotlib import pyplot as plt
+from math import pi, sin, cos, tan
+
+print '-----------------------------------------------------------------------------'
+print '绘制一个简单的图表'
+
+# years = [1950, 1960, 1970, 1980, 1990, 2000, 2010]
+# gdp = [300.2, 543.3, 1075.9, 2862.5, 5979.6, 10289.7, 14958.3]
+#
+# # create a line chart, years on x-axis, gdp on y-axis
+# plt.plot(years, gdp, color='green', marker='o', linestyle='solid')
+#
+# # add a title
+# plt.title("Nominal GDP")
+#
+# # add a label to the y-axis
+# plt.ylabel("Billions of $")
+#
+# plt.show()
+print '-----------------------------------------------------------------------------'
+print '条形图'
+
+# movies = ["Annie Hall", "Ben-Hur", "Casablanca", "Gandhi", "West Side Story"]
+# num_oscars = [5, 11, 3, 8, 10]
+#
+# # bars are by default with 0.8, so we'll add 0.1 to the left corrdinates
+# # so that each bar is centered
+# xs = [i + 0.1 for i, _ in enumerate(movies)]
+#
+# # plot bars with left x-coordinates [xs], heights [num_oscars]
+# plt.bar(xs, num_oscars)
+#
+# plt.ylabel("# of Academy Awards")
+# plt.title("My Favorite Movies")
+#
+# # label x-axis with movie names at bar centers
+# plt.xticks([i + 0.5 for i, _ in enumerate(movies)], movies)
+#
+# plt.show()
+print '-----------------------------------------------------------------------------'
+print '-----------------------------------------------------------------------------'
+print '-----------------------------------------------------------------------------'
+print '-----------------------------------------------------------------------------'
+
+
---
+++ data_science/data_science_from_scratch/03_visualizing_data_demo.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+import numpy as np
+from matplotlib import pyplot as plt
+from math import pi, sin, cos, tan
+
+print '-----------------------------------------------------------------------------'
+print '绘制一个简单的图表'
+
+# x = np.arange(-2*pi, 2*pi, 0.1)
+# y = np.sin(x)
+#
+# # print type(x)
+# # <type 'numpy.ndarray'>
+# # print type(y)
+# # <type 'numpy.ndarray'>
+#
+# plt.plot(x, y)
+# plt.show()
+print '-----------------------------------------------------------------------------'
+print '绘制一个简单的图表'
+
+# x = range(10)
+# y = range(10)
+#
+# # print type(x)
+# # <type 'numpy.ndarray'>
+# # print type(y)
+# # <type 'numpy.ndarray'>
+#
+# plt.plot(x, y)
+# plt.show()
+print '-----------------------------------------------------------------------------'
+print '-----------------------------------------------------------------------------'
+print '-----------------------------------------------------------------------------'
+
