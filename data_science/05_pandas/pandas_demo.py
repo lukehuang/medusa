536f90e8503d945195f8b76a08b4623ca91e7e16
gaohaoning <gaohaoning@unipus.cn>
2019-02-28 18:11:05 +0800

    update code of pandas.

--- data_science/05_pandas/pandas_demo.py
+++ data_science/05_pandas/pandas_demo.py
@@ -2,15 +2,22 @@
 # coding:utf-8
 
 """
-pandas 两种主要的数据类型:
+pandas 两种核心数据结构:
     Series
+        (1维) 带有标签的同构类型数组
     DataFrame
+        (2维) 表格结构，带有标签，大小可变，且可以包含异构的数据列
+
+DataFrame可以看做是Series的容器。即：一个DataFrame中可以包含若干个Series。
 """
 
-import pandas as pd
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 
 print '-----------------------------------------------------------------------------'
 print '-----------------------------------------------------------------------------'
+
+
+
