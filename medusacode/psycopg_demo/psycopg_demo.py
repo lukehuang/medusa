17c3fb990f01776d92757afc6d775aa3e355c906
gaohaoning <gaohaoning@unipus.cn>
2019-04-12 11:30:46 +0800

    use psycopg OK.

--- medusacode/psycopg_demo/psycopg_demo.py
+++ medusacode/psycopg_demo/psycopg_demo.py
@@ -5,14 +5,41 @@
 import psycopg2
 
 
-DATABASE = 'postgresql://postgres:postgres@192.168.100.100:5432/sqlalchemy'
+class PSQL:
+    def __init__(self):
+        self.conn = psycopg2.connect(
+            host='192.168.100.111',
+            port=5432,
+            user='medusa',
+            password='medusa',
+            dbname='medusa'
+        )
+        self.cursor = self.conn.cursor()
+        pass
+
+    def __del__(self):
+        self.cursor.close()
+        self.conn.close()
+        pass
+
+    def sql(self, cmd):
+        self.cursor.execute(str(cmd))
+        result = self.cursor.fetchone()
+        while result:
+            result = self.cursor.fetchone()
+        return result
+
+    def sql_all(self, cmd):
+        self.cursor.execute(str(cmd))
+        result = self.cursor.fetchall()
+        return result
+
 
 print '----------------------------------------------------------------------------------------------------'
-print '----------------------------------------------------------------------------------------------------'
-print '----------------------------------------------------------------------------------------------------'
-print '----------------------------------------------------------------------------------------------------'
-print '----------------------------------------------------------------------------------------------------'
-print '----------------------------------------------------------------------------------------------------'
-print '----------------------------------------------------------------------------------------------------'
-print '----------------------------------------------------------------------------------------------------'
 
+psql = PSQL()
+rows = psql.sql_all('select * from myapp_news;')
+
+for row in rows:
+    print row[0], row[1]
+    pass
